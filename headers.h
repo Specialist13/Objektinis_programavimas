ed342bf6ac4921d2fb256ebd106117208da5a479
Specialist13 <125213612+Specialist13@users.noreply.github.com>
2025-02-07 12:45:41 +0200

    Sukurtas header failas
    Apsirašiau reikiamą struktūra, importavau bibliotekas ir sutvarkiau, kad esminėms funkcijoms nereikėtų rašyti std::

---
+++ headers.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+struct Stud {
+    string vardas, pavarde;
+    vector<int> pazymiai;
+    int egzaminas;
+};
